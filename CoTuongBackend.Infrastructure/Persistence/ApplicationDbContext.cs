4f249eafcfa68670fc42c3539681fc2a2dd46ce6
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-09-01 19:33:52 +0700

    add Migrations and Split to Configuration file

--- CoTuongBackend.Infrastructure/Persistence/ApplicationDbContext.cs
+++ CoTuongBackend.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace CoTuongBackend.Infrastructure.Persistence;
 
@@ -11,29 +12,6 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityR
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Room>()
-            .HasOne(m => m.HostUser)
-            .WithMany(u => u.Rooms)
-            .HasForeignKey(m => m.HostUserId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Match>()
-            .HasOne(m => m.HostUser)
-            .WithMany(u => u.HostedMatches)
-            .HasForeignKey(m => m.HostUserId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Match>()
-            .HasOne(m => m.OpponentUser)
-            .WithMany(u => u.OpponentMatches)
-            .HasForeignKey(m => m.OpponentUserId)
-            .OnDelete(DeleteBehavior.Restrict);
-
-        modelBuilder.Entity<Match>()
-            .HasOne(m => m.Room)
-            .WithMany(u => u.Matches)
-            .HasForeignKey(m => m.RoomId)
-            .OnDelete(DeleteBehavior.Restrict);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
     }
 }
---
+++ CoTuongBackend.Infrastructure/Persistence/Configurations/RoomConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using CoTuongBackend.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CoTuongBackend.Infrastructure.Persistence.Configurations;
+
+public class RoomConfiguration : IEntityTypeConfiguration<Room>
+{
+    public void Configure(EntityTypeBuilder<Room> builder)
+    {
+        builder
+            .Property(r => r.CountUser)
+            .HasDefaultValue(1);
+
+        builder
+            .HasOne(m => m.HostUser)
+            .WithMany(u => u.Rooms)
+            .HasForeignKey(m => m.HostUserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
