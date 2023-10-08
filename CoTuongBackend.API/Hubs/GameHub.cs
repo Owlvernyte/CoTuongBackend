81fd4afa0afdcc1c3d7254da552e9a6fd0215ec4
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-08 20:55:33 +0700

    refactor: Update Boards and Update move method

--- CoTuongBackend.API/Hubs/GameHub.cs
+++ CoTuongBackend.API/Hubs/GameHub.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using CoTuongBackend.Domain.Entities.Games;
+using Microsoft.AspNetCore.SignalR;
 
 namespace CoTuongBackend.API.Hubs;
 
 public class GameHub : Hub<IGameHubClient>
 {
+    public static Dictionary<string, Board> Boards { get; set; } = new Dictionary<string, Board>
+    {
+        ["RoomId"] = new Board
+        {
+            Squares = new Piece?[9, 10]
+            {
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+                { null, null, null, null, null, null, null, null, null, null},
+            }
+        }
+    };
     public override Task OnConnectedAsync()
     {
+        // Room Id
+
         Console.WriteLine("Nguoi choi " + Context.ConnectionId + " da ket noi vao hub");
 
         Clients.All.Joined("Nguoi choi " + Context.ConnectionId + " da tham gia phong!");
@@ -20,13 +41,26 @@ public class GameHub : Hub<IGameHubClient>
 
         return base.OnDisconnectedAsync(exception);
     }
-    public Task Move(string source, string destination)
+    public Task Move(Coordinate source, Coordinate destination)
     {
         // connection.invoke("Move", "a1", "a2");
 
+        var roomId = "RoomId";
+
+        Piece piece = Boards[roomId].Squares[source.X, source.Y]!;
+
+        var isValid = piece.IsValidMove(destination, Boards[roomId]);
+
+        // Move
+        if (!isValid) return Task.CompletedTask;
+
+        Boards[roomId].Squares[destination.X, destination.Y] = Boards[roomId].Squares[source.X, source.Y];
+
+        Boards[roomId].Squares[source.X, source.Y] = null;
+
         Console.WriteLine(source + " " + destination);
 
-        Clients.All.Moved("Nguoi choi da di chuyen tu " + source + " den " + destination);
+        Clients.All.Moved(source, destination, piece.IsRed);
 
         return Task.CompletedTask;
     }
--- CoTuongBackend.API/Hubs/IGameHubClient.cs
+++ CoTuongBackend.API/Hubs/IGameHubClient.cs
@@ -1,8 +1,10 @@
-﻿namespace CoTuongBackend.API.Hubs;
+﻿using CoTuongBackend.Domain.Entities.Games;
+
+namespace CoTuongBackend.API.Hubs;
 
 public interface IGameHubClient
 {
     Task Joined(string message);
     Task Left(string message);
-    Task Moved(string result);
+    Task Moved(Coordinate source, Coordinate destination, bool turn);
 }
