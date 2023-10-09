51854cd7761e557178e2e3498fd24c22bc5db02b
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-09 10:29:21 +0700

    feat: Add AccountDto

--- CoTuongBackend.Application/Users/ChagePasswordDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoTuongBackend.Application.Users;
-
-public record ChagePasswordDto
-{
-    public required string UserNameOrEmail { get; set; }
-    public required string NewPassword { get; set; }
-    public required string ConfirmPassword { get; set; }
-    public required string OldPassword { get; set; }
-}
