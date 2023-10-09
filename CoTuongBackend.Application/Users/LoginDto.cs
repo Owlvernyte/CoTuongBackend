69b0aa23d3d238ecd68a82712f20f303ff673cb9
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-09 10:30:17 +0700

    feat: Add LoginDto

---
+++ CoTuongBackend.Application/Users/LoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace CoTuongBackend.Application.Users;
+
+public record LoginDto
+{
+    public required string UserNameOrEmail { get; set; }
+    public required string Password { get; set; }
+}
--- CoTuongBackend.Application/Users/RegisterDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoTuongBackend.Application.Users;
-
-public record RegisterDto
-{
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-    public required string Email { get; set; }
-    public required string ConfirmPassword { get; set; }
-}
