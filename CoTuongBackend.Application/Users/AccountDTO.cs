db775e8bcc363d1d30f8cb8231560a02b0413da4
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-09 10:28:36 +0700

    feat: Remove AccountDTO

--- CoTuongBackend.Application/Users/AccountDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoTuongBackend.Application.Users;
-
-public record AccountDto
-{
-    public Guid Id { get; set; }
-    public required string UserName { get; set; }
-    public required string Email { get; set; }
-    public string? Token { get; set; }
-}
