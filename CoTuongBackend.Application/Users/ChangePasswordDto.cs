48d28ddb3f7c4f0f134103c5e6a83f4dcbdcd717
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-09 10:29:50 +0700

    feat: Add ChangePasswordDto

---
+++ CoTuongBackend.Application/Users/ChangePasswordDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CoTuongBackend.Application.Users;
+
+public record ChagePasswordDto
+{
+    public required string UserNameOrEmail { get; set; }
+    public required string NewPassword { get; set; }
+    public required string ConfirmPassword { get; set; }
+    public required string OldPassword { get; set; }
+}
--- CoTuongBackend.Application/Users/LoginDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CoTuongBackend.Application.Users;
-
-public record LoginDto
-{
-    public required string UserNameOrEmail { get; set; }
-    public required string Password { get; set; }
-}
