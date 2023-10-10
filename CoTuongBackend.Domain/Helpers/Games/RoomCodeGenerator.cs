211d1a1ee5ec380cc06bae5c990cd6298698b9a7
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-10 14:01:59 +0700

    feat: Add RandomStringGenerator and RoomCodeGenerator

---
+++ CoTuongBackend.Domain/Helpers/Games/RoomCodeGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace CoTuongBackend.Domain.Helpers.Games;
+
+public static class RoomCodeGenerator
+{
+    const string _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    const int _codeLength = 6;
+    public static string Generate()
+        => RandomStringGenerator.Generate(_chars, _codeLength);
+}
