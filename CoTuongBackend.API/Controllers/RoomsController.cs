7ecb8918465ecf0eb76b68fe2b25c4add428f21c
Slimaeus <nguyenhongthai28042002@gmail.com>
2023-10-10 14:36:58 +0700

    refactor: Use 2d list for Squares

---
+++ CoTuongBackend.API/Controllers/RoomsController.cs
@@ -0,0 +1,16 @@
+﻿using CoTuongBackend.API.Hubs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoTuongBackend.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class RoomsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        await Task.Delay(0);
+        return Ok(GameHub.Boards.Select(x => x.Key).ToList());
+    }
+}
