ca34b537b2fdc769208cefef4452487c5625367f
phamtruong7302 <truongpham7302@gmail.com>
2023-10-18 15:50:59 +0700

    feat: add canon logic

--- CoTuongBackend.Domain/Entities/Games/Pieces/Cannon.cs
+++ CoTuongBackend.Domain/Entities/Games/Pieces/Cannon.cs
@@ -14,7 +14,46 @@ public sealed class Cannon : Piece
         bool isBaseValidMove = base.IsValidMove(destinationCoordinate, board);
         if (!isBaseValidMove)
             return false;
+        if(destinationCoordinate.X != Coord.X)
+            return CheckRow(destinationCoordinate, board);
+        if(destinationCoordinate.Y != Coord.Y)
+            return CheckColums(destinationCoordinate, board);
+        return true;
+    }
 
+    public bool CheckRow(Coordinate destinationCoordinate, Board board)
+    {
+        if (Coord.X < destinationCoordinate.X)
+        {
+            for (int i = Coord.X; i < destinationCoordinate.X; i++)
+            {
+                if (board.Squares[i][destinationCoordinate.Y] != null )
+                    return false;
+            }
+        }
+        for(int i = Coord.X; i > destinationCoordinate.X; i--)
+        {
+            if (board.Squares[i][destinationCoordinate.Y] != null)
+                return false;
+        }
+        return true;
+    }
+
+    public bool CheckColums(Coordinate destinationCoordinate, Board board)
+    {
+        if (Coord.Y < destinationCoordinate.Y)
+        {
+            for (int i = Coord.Y; i < destinationCoordinate.Y; i++)
+            {
+                if (board.Squares[destinationCoordinate.X][i] != null)
+                    return false;
+            }
+        }
+        for (int i = Coord.Y; i > destinationCoordinate.Y; i--)
+        {
+            if (board.Squares[destinationCoordinate.X][i] != null)
+                return false;
+        }
         return true;
     }
 }
